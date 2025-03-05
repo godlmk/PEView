a24c07add1158cb1b733eebb5a837655d68668e8
lmk <liumingkai2020@outlook.com>
2025-03-05 15:10:42 +0800

    update reademe and some code

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# reverse
+# PEView
 
-PE相关工具
+PE相关工具:
+1. kml_shell是壳子程序
+2. loadPE文件夹是一个windows界面程序，包含PE解析逻辑和加壳程序。
 
 都是用VS开发，x86模式编译
--- kml_shell/kml_shell.cpp
+++ kml_shell/kml_shell.cpp
@@ -7,131 +7,78 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	// 读取自己的filebuffer
-	// 得到当前的路径
-	TCHAR path[MAX_PATH] = { 0 };
-	if (GetModuleFileName(NULL, path, MAX_PATH) == 0)
-	{
-		println("读取当前path失败，");
-		return -1;
-	}
-	void* file_buffer;
-	ReadFileBuffer(path, &file_buffer);
+	//--------------------------------------解密过程--------------------------------------
+	//获取当前程序运行路径
+	char FilePathSelf[255] = { 0 };
+	GetModuleFileName(NULL, FilePathSelf, 255);
 
-	// 读取src的数据，储存在最后一个节里
-	PIMAGE_DOS_HEADER dos_header = GetDosHeader(file_buffer);
-	PIMAGE_NT_HEADERS nt_headers = GetNTHeader(file_buffer, dos_header);
-	PIMAGE_FILE_HEADER file_header = &nt_headers->FileHeader;
-	PIMAGE_OPTIONAL_HEADER op_header = &nt_headers->OptionalHeader;
-	int section_count = file_header->NumberOfSections;
-	PIMAGE_SECTION_HEADER last_section = IMAGE_FIRST_SECTION(nt_headers) + (section_count - 1);
+	// 1、读取当前壳子程序本身 数据
+	PVOID file_buffer_shell;
+	DWORD size_shell = ReadFileBuffer(FilePathSelf, &file_buffer_shell);
+	PIMAGE_DOS_HEADER dos_header_shell = GetDosHeader(file_buffer_shell);
+	PIMAGE_NT_HEADERS nt_header_shell = GetNTHeader(file_buffer_shell, dos_header_shell);
+	PIMAGE_OPTIONAL_HEADER optional_header_shell = &nt_header_shell->OptionalHeader;
+	PIMAGE_SECTION_HEADER last_section_shell = IMAGE_FIRST_SECTION(nt_header_shell) +
+		(nt_header_shell->FileHeader.NumberOfSections - 1);
+	DWORD dwImageBase_shell = optional_header_shell->ImageBase;
 
-	// 读取出来保存到另一块区域
-	void* src_file_buffer = malloc(last_section->SizeOfRawData);
-	memcpy(src_file_buffer, ((PBYTE)file_buffer + last_section->PointerToRawData), last_section->SizeOfRawData);
+	// 2、解密源文件,获取源文件的imagebase sizeofimage数据
+	DWORD dwImageBase_src, dwSizeOfImage_src, dwEOP_src;
+	PVOID file_buffer_src = (PVOID)((PBYTE)file_buffer_shell + last_section_shell->VirtualAddress);
+	BOOL bRet = GetPEInfoFromFile(file_buffer_src, &dwImageBase_src, &dwSizeOfImage_src, &dwEOP_src);
 
-	// 以挂起方式创建该shell进程
+	// 3、拉伸PE  
+	PVOID pImageBufferSrc = FileBuffer2MemoryBuffer(file_buffer_src);
+
+	// 4、以挂起方式运行壳程序进程
 	STARTUPINFO si = { 0 };
-	PROCESS_INFORMATION pi;
 	si.cb = sizeof(si);
-	if (!CreateProcess(NULL, path, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi))
-	{
-		println("CreateProcess failed, error: {}", GetLastError());
-		return -1;
-	}
-	CONTEXT context;
-	context.ContextFlags = CONTEXT_FULL;
-	if (!GetThreadContext(pi.hThread, &context))
-	{
-		println("GetThreadContext failed, error: {}", GetLastError());
-		return -1;
-	}
+	PROCESS_INFORMATION pi = { 0 };
+	::CreateProcess(FilePathSelf, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
 
-	// 卸载创建出进程的壳，也就是取消映射,需要手动从动态库中找到该函数
-	HMODULE ntdll = LoadLibrary(TEXT("ntdll.dll"));
-	if (ntdll == NULL)
-	{
-		println("LoadLibrary ntdll failed");
-		return -1;
-	}
-	using NtUnmapViewOfSectionfn = ULONG(WINAPI*)(HANDLE ProcessHandle, PVOID BaseAddress);
-	NtUnmapViewOfSectionfn fn = (NtUnmapViewOfSectionfn)GetProcAddress(ntdll, "NtUnmapViewOfSection");
-	if (fn == NULL)
-	{
-		println("GetProcAddress NtUnmapViewOfSection failed");
-		return -1;
-	}
-	DWORD dwVictimBaseAddr{ 0 };
-	if (ReadProcessMemory(pi.hProcess, (LPCVOID)(context.Ebx + 8),
-		&dwVictimBaseAddr, sizeof(PVOID), NULL) == 0)
-	{
-		println("ReadProcessMemory  failed");
-		return -1;
-	}
-	ULONG status = fn(pi.hProcess, (PVOID)dwVictimBaseAddr);
-	println("Unmap status: {}", status);
 
-	// 在shell的src位置分配足够的空间
-	DWORD src_sizeOfImage, src_imageBase, src_eop;
-	BOOL ans = GetPEInfoFromFile(src_file_buffer, &src_imageBase, &src_sizeOfImage, &src_eop);
-	if (!ans)
+	//5、卸载外壳程序的文件镜像
+	using NtUnmapViewOfSectionFn = LONG(WINAPI*)(HANDLE, PVOID);
+	HMODULE hNtModule = LoadLibrary("ntdll.dll");
+	NtUnmapViewOfSectionFn NtUnmapViewOfSection = (NtUnmapViewOfSectionFn)GetProcAddress(hNtModule, "NtUnmapViewOfSection");
+	if (NtUnmapViewOfSection == NULL)
 	{
-		println("GetPEInfoFromFile failed");
+		std::println("get NtUnmapViewOfSectionFn failed");
 		return -1;
 	}
-	std::println("imagebase is 0x{:X}, sizeofimage is 0x{:X}, eop is 0x{:X}", src_imageBase, src_sizeOfImage,
-		src_eop);
-	PVOID allocBase = VirtualAllocEx(pi.hProcess, (PVOID)src_imageBase, src_sizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-	if (allocBase == NULL)
+	if (NtUnmapViewOfSection(pi.hProcess, (PVOID)dwImageBase_shell) != 0)
 	{
-		println("VirtualAllocEx failed, error: {}", GetLastError());
+		std::println("NtUnmapViewOfSectionFn failed");
 		return -1;
 	}
-
-	// 拷贝拉伸后的src
-	void* src_memory_buffer = FileBuffer2MemoryBuffer(src_file_buffer);
-	if (src_memory_buffer == NULL)
+	FreeLibrary(hNtModule);
+	//6、在指定的位置(src的ImageBase)申请指定大小(src的SizeOfImage)的内存(VirtualAllocEx)
+	LPVOID status = VirtualAllocEx(pi.hProcess, (PVOID)dwImageBase_src, dwSizeOfImage_src,
+		MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (status == NULL)
 	{
-		println("FileBuffer2MemoryBuffer failed");
+		std::println("alloc fail");
 		return -1;
 	}
-	if (!WriteProcessMemory(pi.hProcess, (PVOID)src_imageBase, src_memory_buffer, src_sizeOfImage, NULL))
-	{
-		println("WriteProcessMemory failed, error: {}", GetLastError());
-		return -1;
-	}
+	//7、如果成功，将Src的PE文件拉伸 复制到该空间中
+	::WriteProcessMemory(pi.hProcess, (PVOID)dwImageBase_src, pImageBufferSrc, dwSizeOfImage_src, NULL);
 
-	// 修改context
-	// 替换PEB中的基地址
+	//8、如果申请空间失败，但有重定位表：在任意位置申请空间，然后将PE文件拉伸、复制、修复重定位表。
+	//	// 9、如果第6步申请空间失败，并且还没有重定位表，直接返回：失败.
 
-	DWORD dwImageBase = op_header->ImageBase;
-	BOOL bRet = WriteProcessMemory(pi.hProcess, (PVOID)(context.Ebx + 8),
-		(LPCVOID)&dwImageBase, sizeof(PVOID), NULL);
-	if (!bRet)
-	{
-		return -1;
-	}
-
-	// 设置入口点
-	context.Eax = dwImageBase + op_header->AddressOfEntryPoint;
-
-	// 设置回context
-	if (!SetThreadContext(pi.hThread, &context))
-	{
-		println("SetThreadContext failed, error: {}", GetLastError());
-		return -1;
-	}
-
-	// 恢复线程
-	if (ResumeThread(pi.hThread) == -1)
-	{
-		println("ResumeThread failed, error: {}", GetLastError());
-		return -1;
-	}
-
-	println("Process resumed successfully");
+	// 10、修改外壳程序的Context:
+	CONTEXT cont;
+	cont.ContextFlags = CONTEXT_FULL;
+	::GetThreadContext(pi.hThread, &cont);
+	// 设置imagebase
+	::WriteProcessMemory(pi.hProcess, (PVOID)(cont.Ebx + 8), &dwImageBase_src, 4, NULL);
+	// 设置eop
+	cont.Eax = dwImageBase_src + dwEOP_src;
+	::SetThreadContext(pi.hThread, &cont);
+	//恢复线程
+	::ResumeThread(pi.hThread);
 	return 0;
 }
 
